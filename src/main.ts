23f9f70ca3b4334a22446dd5174eb1bb158f88a7
TomasMadariaga <tomas.madariaga@municipiodetandil.edu.ar>
2025-02-22 16:05:01 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || 'https://cruzpatagonia.com.ar',
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
